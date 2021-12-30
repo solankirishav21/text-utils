149b15fbb73bb4357d48f1225db6eac9b3e987a6
solankirishav21 <btech10645.20@bitmesra.ac.in>
2021-12-30 11:36:35 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./Components/Navbar"
+import Textarea from "./Components/Textarea"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar title="Text-Utils" />
+      <div className="container my-3">
+      <Textarea head="Enter your Text" />
+      </div>
+
+
+    </>
   );
 }
 
---
+++ src/Components/Textarea.js
@@ -0,0 +1,96 @@
+// import React from "react";
+import React, { useState } from "react";
+
+export default function Textarea(props) {
+  const [text, setText] = useState("");
+
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleUpperCase = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+
+  const handleLowerCase = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+
+  const handleCopy = () => {
+    let text = document.getElementById("textarea");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  };
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  };
+  const handleClearText = () => {
+    setText("");
+  };
+
+  return (
+    <>
+      <div className="container">
+        <h1>{props.head}</h1>
+        <div className="mb-3">
+          <label htmlFor="inputarea" className="form-label"></label>
+
+          <textarea
+            className="form-control"
+            rows="6"
+            value={text}
+            onChange={handleOnChange}
+            id="textarea"
+          ></textarea>
+        </div>
+        <button
+          type="button"
+          className="btn btn-secondary mx-2"
+          onClick={handleUpperCase}
+        >
+          Convert to UPPERCASE
+        </button>
+        <button
+          type="button"
+          className="btn btn-secondary mx-2"
+          onClick={handleLowerCase}
+        >
+          Convert to LOWERCASE
+        </button>
+        <button
+          type="button"
+          className="btn btn-secondary mx-2"
+          onClick={handleCopy}
+        >
+          Copy Text
+        </button>
+        <button
+          type="button"
+          className="btn btn-secondary mx-2"
+          onClick={handleExtraSpaces}
+        >
+          Remove Extra Spaces
+        </button>
+        <button
+          type="button"
+          className="btn btn-secondary mx-2"
+          onClick={handleClearText}
+        >
+          Clear
+        </button>
+      </div>
+      <div className="container my-3">
+        <h2> Text Summary</h2>
+        <p>
+          {" "}
+          {text.split(" ").length} words and {text.length} characters
+        </p>
+        <h3> Preview</h3>
+        <p>{text.length>0?text:"Enter the text to preview" }</p>
+      </div>
+    </>
+  );
+}
